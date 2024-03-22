e979d283e56310b5ade6a9a1df2dd3753a0fba7a
MoonRivers21 <162838243+MoonRivers21@users.noreply.github.com>
2024-03-22 09:27:46 +0800

    Add files via upload

---
+++ app/Console/Commands/DeleteOldTasks.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Task;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class DeleteOldTasks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tasks:delete-old';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Trash tasks older than 30 days';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $thresholdDate = Carbon::now()->subDays(30);
+        Task::where('created_at', '<', $thresholdDate)->delete();
+        $this->info('Old tasks deleted successfully.');
+    }
+}
